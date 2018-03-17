4b1385a4d49e9c2b6d4e12ec38e3e7784708d32b
shiro <shiro.mns@gmail.com>
2018-03-17 15:52:26 +0900

    [追加]既存のデータ小屋のjavascript追加、vendor/assetsにパスを通す(Rails5は手動で通す必要がある)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,62 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require turbolinks
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require turbolinks
+//= require "jquery.cleanQuery"
 //= require_tree .
+
+// 説明文の開閉処理
+function example_open(flg) {
+	var example=document.getElementById("example");
+	var example_open=document.getElementById("example_open");
+	var example_close=document.getElementById("example_close");
+
+    if(!example || !example_open){return;}
+
+	if(flg) {
+		example.style.display='inline';
+		example_open.style.display='none';
+		example_close.style.display='inline';
+
+	}else{
+		example.style.display='none';
+		example_open.style.display='inline';
+		example_close.style.display='none';
+	}
+};
+
+// 検索フォームの開閉処理
+function search_open(flg) {
+	var table=document.getElementById("search_table");
+	var close=document.getElementById("search_close");
+
+    if(!table || !close){return;}
+
+	if(flg) {
+		table.style.display='table';
+		close.style.display='none';
+
+	}else{
+		table.style.display='none';
+		close.style.display='table';
+	}
+};
+
+// ページ移動時の発火処理
+var turboReady = function(){
+	var url     = location.href;
+    var params  = url.split("?");
+
+	if(params[1]){
+		search_open(false);
+        example_open(false);
+	}
+
+    $('FORM').cleanQuery();
+};
+
+$(document).on('turbolinks:load', turboReady);
+
