6dc7c54e6d8585e6405f5b9d48ee1696feda9347
shiro <shiro.mns@gmail.com>
2018-03-17 15:37:26 +0900

    [追加]トップページ、bootstrapレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'top_page/index'
+  root 'top_page#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
