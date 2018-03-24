8cb57e921839421c0d47e360144d99a6d2b79901
shiro <shiro.mns@gmail.com>
2018-03-24 11:46:49 +0900

    [追加]scaffoldで作成したファイルを調整、固有名詞の関連付け

--- app/controllers/skill_data_controller.rb
+++ app/controllers/skill_data_controller.rb
@@ -5,47 +5,38 @@ class SkillDataController < ApplicationController
   # GET /skill_data
   def index
     param_set
-    @count	= SkillDatum.includes([:p_name]).search(params[:q]).result.count()
-    @search	= SkillDatum.includes([:p_name]).page(params[:page]).search(params[:q])
+    @count	= SkillDatum.includes([:timing_name, :target_name, :property_name, :element_name]).search(params[:q]).result.count()
+    @search	= SkillDatum.includes([:timing_name, :target_name, :property_name, :element_name]).page(params[:page]).search(params[:q])
     @search.sorts = 'id asc' if @search.sorts.empty?
     @skill_data	= @search.result.per(50)
   end
 
   def param_set
-    @last_result = Name.maximum('result_no')
-    params["result_no_form"] = params["result_no_form"] ? params["result_no_form"] : sprintf('%d',@last_result)
     params[:q]  = params[:q] ? params[:q] : {}
     
-    reference_text_assign(params, "p_name_name", "p_name_form")
-        reference_number_assign(params, "skill_id", "skill_id_form")
-        reference_number_assign(params, "name", "name_form")
-        reference_number_assign(params, "at", "at_form")
-        reference_number_assign(params, "ct", "ct_form")
-        reference_number_assign(params, "timing", "timing_form")
-        reference_number_assign(params, "mp", "mp_form")
-        reference_number_assign(params, "target", "target_form")
-        reference_number_assign(params, "range", "range_form")
-        reference_number_assign(params, "property", "property_form")
-        reference_number_assign(params, "element", "element_form")
-        reference_number_assign(params, "text", "text_form")
+    reference_number_assign(params, "skill_id", "skill_id_form")
+    reference_text_assign(params, "name", "name_form")
+    reference_number_assign(params, "at", "at_form")
+    reference_number_assign(params, "ct", "ct_form")
+    reference_text_assign(params, "timing_name_name", "timing_form")
+    reference_number_assign(params, "mp", "mp_form")
+    reference_text_assign(params, "target_name_name", "target_form")
+    reference_number_assign(params, "range", "range_form")
+    reference_text_assign(params, "property_name_name", "property_form")
+    reference_text_assign(params, "element_name_name", "element_form")
+    reference_text_assign(params, "text", "text_form")
         
-    @p_name_form = params["p_name_form"]
-        @skill_id_form = params["skill_id_form"]
-        @name_form = params["name_form"]
-        @at_form = params["at_form"]
-        @ct_form = params["ct_form"]
-        @timing_form = params["timing_form"]
-        @mp_form = params["mp_form"]
-        @target_form = params["target_form"]
-        @range_form = params["range_form"]
-        @property_form = params["property_form"]
-        @element_form = params["element_form"]
-        @text_form = params["text_form"]
-        
-    show_sub_hash =  {"show_main"=> @show_main,"show_sub" => @show_sub}
-    sub_no_set(params, show_sub_hash)
-    @show_main = show_sub_hash["show_main"]
-    @show_sub = show_sub_hash["show_sub"]
+    @skill_id_form = params["skill_id_form"]
+    @name_form = params["name_form"]
+    @at_form = params["at_form"]
+    @ct_form = params["ct_form"]
+    @timing_form = params["timing_form"]
+    @mp_form = params["mp_form"]
+    @target_form = params["target_form"]
+    @range_form = params["range_form"]
+    @property_form = params["property_form"]
+    @element_form = params["element_form"]
+    @text_form = params["text_form"]
   end
   # GET /skill_data/1
   #def show
--- app/models/skill_datum.rb
+++ app/models/skill_datum.rb
@@ -1,2 +1,6 @@
 class SkillDatum < ApplicationRecord
+	belongs_to :timing_name,   :foreign_key => :timing,   :primary_key => :proper_id, :class_name => 'ProperName'
+	belongs_to :target_name,   :foreign_key => :target,   :primary_key => :proper_id, :class_name => 'ProperName'
+	belongs_to :property_name, :foreign_key => :property, :primary_key => :proper_id, :class_name => 'ProperName'
+	belongs_to :element_name,  :foreign_key => :element,  :primary_key => :proper_id, :class_name => 'ProperName'
 end
