5692fb2ac503374ddad03a7c0ea383b10a7243d9
shiro <shiro.mns@gmail.com>
2018-03-25 13:26:35 +0900

    [追加]戦型-習得可能技一覧に技情報を表示

--- app/controllers/learnable_skills_controller.rb
+++ app/controllers/learnable_skills_controller.rb
@@ -5,8 +5,8 @@ class LearnableSkillsController < ApplicationController
   # GET /learnable_skills
   def index
     param_set
-    @count	= LearnableSkill.includes([:job_name, :skill_data]).search(params[:q]).result.count()
-    @search	= LearnableSkill.includes([:job_name, :skill_data]).page(params[:page]).search(params[:q])
+    @count	= LearnableSkill.includes(:job_name, skill_data: [:timing_name, :target_name, :property_name, :element_name]).search(params[:q]).result.count()
+    @search	= LearnableSkill.includes(:job_name, skill_data: [:timing_name, :target_name, :property_name, :element_name]).page(params[:page]).search(params[:q])
     @search.sorts = 'id asc' if @search.sorts.empty?
     @learnable_skills	= @search.result.per(50)
   end
@@ -19,12 +19,30 @@ class LearnableSkillsController < ApplicationController
     reference_number_assign(params, "sp", "sp_form")
     reference_number_assign(params, "skill_no", "skill_no_form")
     reference_text_assign(params, "skill_data_name", "skill_id_form")
-    
+    reference_number_assign(params, "skill_data_at", "at_form")
+    reference_number_assign(params, "skill_data_ct", "ct_form")
+    reference_text_assign(params, "skill_data_timing_name_name", "timing_form")
+    reference_number_assign(params, "skill_data_mp", "mp_form")
+    reference_text_assign(params, "skill_data_target_name_name", "target_form")
+    reference_number_assign(params, "skill_data_range", "range_form")
+    reference_text_assign(params, "skill_data_property_name_name", "property_form")
+    reference_text_assign(params, "skill_data_element_name_name", "element_form")
+    reference_text_assign(params, "skill_data_text", "text_form")
+        
     @chara_type_form = params["chara_type_form"]
     @job_id_form = params["job_id_form"]
     @sp_form = params["sp_form"]
     @skill_no_form = params["skill_no_form"]
     @skill_id_form = params["skill_id_form"]
+    @at_form = params["at_form"]
+    @ct_form = params["ct_form"]
+    @timing_form = params["timing_form"]
+    @mp_form = params["mp_form"]
+    @target_form = params["target_form"]
+    @range_form = params["range_form"]
+    @property_form = params["property_form"]
+    @element_form = params["element_form"]
+    @text_form = params["text_form"]
         
     params[:q]["chara_type_eq_any"]  = params[:q]["chara_type_eq_any"] ? params[:q]["chara_type_eq_any"] : []
 
@@ -40,6 +58,12 @@ class LearnableSkillsController < ApplicationController
 
     @show_main = params["show_main"]
     @show_sub = params["show_sub"]
+
+    params[:q]["skill_data_range_eq_any"] = []
+    if params["equip_range"] == "on" then params[:q]["skill_data_range_eq_any"].push(-2) end
+    if params["no_range"] == "on" then params[:q]["skill_data_range_eq_any"].push(-1) end
+    @equip_range = params["equip_range"]
+    @no_range  = params["no_range"]
   end
   # GET /learnable_skills/1
   #def show
