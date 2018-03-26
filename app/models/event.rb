7eecd5c344fe1e60afc3dcc072ec1cad9e1a7899
shiro <shiro.mns@gmail.com>
2018-03-26 11:37:05 +0900

    [追加]scaffoldで作成したファイルを調整、イベント名、状況、説明の関連付け

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+	belongs_to :p_name,	    :foreign_key => [:e_no, :result_no, :sub_no, :generate_no], :primary_key => [:e_no, :result_no, :sub_no, :generate_no], :class_name => 'Name'
+	belongs_to :event_name, :foreign_key => :event,    :primary_key => :proper_id, :class_name => 'ProperName'
+	belongs_to :flag_name,  :foreign_key => :flag,    :primary_key => :proper_id, :class_name => 'ProperName'
+	belongs_to :text_name,  :foreign_key => :text,    :primary_key => :proper_id, :class_name => 'ProperName'
 end
