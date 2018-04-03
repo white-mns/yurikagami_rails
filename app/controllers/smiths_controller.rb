2b6e2834714fb796e5db90a3ab6c3838842ff632
shiro <shiro.mns@gmail.com>
2018-04-03 13:07:08 +0900

    [追加]scaffoldで作成したファイルを調整、各種情報の関連付け (N+1問題は、引数指定形式の制限上完全には解決できない)

--- app/controllers/smiths_controller.rb
+++ app/controllers/smiths_controller.rb
@@ -5,8 +5,8 @@ class SmithsController < ApplicationController
   # GET /smiths
   def index
     param_set
-    @count	= Smith.includes([:p_name]).search(params[:q]).result.count()
-    @search	= Smith.includes([:p_name]).page(params[:page]).search(params[:q])
+    @count	= Smith.includes(:p_name, :main_material_name, :sub_material_1_name, :sub_material_2_name, :sub_material_3_name, :sub_material_4_name, :main_material, :sub_material_1, :sub_material_2, :sub_material_3, :sub_material_4, :result, source: [:kind_name, :ability_name]).search(params[:q]).result.count()
+    @search	= Smith.includes(:p_name, :main_material_name, :sub_material_1_name, :sub_material_2_name, :sub_material_3_name, :sub_material_4_name, :main_material, :sub_material_1, :sub_material_2, :sub_material_3, :sub_material_4, :result, source: [:kind_name, :ability_name]).page(params[:page]).search(params[:q])
     @search.sorts = 'id asc' if @search.sorts.empty?
     @smiths	= @search.result.per(50)
   end
@@ -17,46 +17,46 @@ class SmithsController < ApplicationController
     params[:q]  = params[:q] ? params[:q] : {}
     
     reference_text_assign(params, "p_name_name", "p_name_form")
-        reference_number_assign(params, "result_no", "result_no_form")
-        reference_number_assign(params, "generate_no", "generate_no_form")
-        reference_number_assign(params, "last_result_no", "last_result_no_form")
-        reference_number_assign(params, "last_generate_no", "last_generate_no_form")
-        reference_number_assign(params, "party_no", "party_no_form")
-        reference_number_assign(params, "e_no", "e_no_form")
-        reference_number_assign(params, "sub_no", "sub_no_form")
-        reference_number_assign(params, "result_i_no", "result_i_no_form")
-        reference_number_assign(params, "source_i_no", "source_i_no_form")
-        reference_number_assign(params, "main_material_i_no", "main_material_i_no_form")
-        reference_number_assign(params, "sub_material_1_i_no", "sub_material_1_i_no_form")
-        reference_number_assign(params, "sub_material_2_i_no", "sub_material_2_i_no_form")
-        reference_number_assign(params, "sub_material_3_i_no", "sub_material_3_i_no_form")
-        reference_number_assign(params, "sub_material_4_i_no", "sub_material_4_i_no_form")
-        reference_number_assign(params, "main_material_name_id", "main_material_name_id_form")
-        reference_number_assign(params, "sub_material_1_name_id", "sub_material_1_name_id_form")
-        reference_number_assign(params, "sub_material_2_name_id", "sub_material_2_name_id_form")
-        reference_number_assign(params, "sub_material_3_name_id", "sub_material_3_name_id_form")
-        reference_number_assign(params, "sub_material_4_name_id", "sub_material_4_name_id_form")
+    reference_number_assign(params, "result_no", "result_no_form")
+    reference_number_assign(params, "generate_no", "generate_no_form")
+    reference_number_assign(params, "last_result_no", "last_result_no_form")
+    reference_number_assign(params, "last_generate_no", "last_generate_no_form")
+    reference_number_assign(params, "party_no", "party_no_form")
+    reference_number_assign(params, "e_no", "e_no_form")
+    reference_number_assign(params, "sub_no", "sub_no_form")
+    reference_number_assign(params, "result_i_no", "result_i_no_form")
+    reference_number_assign(params, "source_i_no", "source_i_no_form")
+    reference_number_assign(params, "main_material_i_no", "main_material_i_no_form")
+    reference_number_assign(params, "sub_material_1_i_no", "sub_material_1_i_no_form")
+    reference_number_assign(params, "sub_material_2_i_no", "sub_material_2_i_no_form")
+    reference_number_assign(params, "sub_material_3_i_no", "sub_material_3_i_no_form")
+    reference_number_assign(params, "sub_material_4_i_no", "sub_material_4_i_no_form")
+    reference_text_assign(params, "main_material_name", "main_material_name_id_form")
+    reference_text_assign(params, "sub_material_1_name", "sub_material_1_name_id_form")
+    reference_text_assign(params, "sub_material_2_name", "sub_material_2_name_id_form")
+    reference_text_assign(params, "sub_material_3_name", "sub_material_3_name_id_form")
+    reference_text_assign(params, "sub_material_4_name", "sub_material_4_name_id_form")
         
     @p_name_form = params["p_name_form"]
-        @result_no_form = params["result_no_form"]
-        @generate_no_form = params["generate_no_form"]
-        @last_result_no_form = params["last_result_no_form"]
-        @last_generate_no_form = params["last_generate_no_form"]
-        @party_no_form = params["party_no_form"]
-        @e_no_form = params["e_no_form"]
-        @sub_no_form = params["sub_no_form"]
-        @result_i_no_form = params["result_i_no_form"]
-        @source_i_no_form = params["source_i_no_form"]
-        @main_material_i_no_form = params["main_material_i_no_form"]
-        @sub_material_1_i_no_form = params["sub_material_1_i_no_form"]
-        @sub_material_2_i_no_form = params["sub_material_2_i_no_form"]
-        @sub_material_3_i_no_form = params["sub_material_3_i_no_form"]
-        @sub_material_4_i_no_form = params["sub_material_4_i_no_form"]
-        @main_material_name_id_form = params["main_material_name_id_form"]
-        @sub_material_1_name_id_form = params["sub_material_1_name_id_form"]
-        @sub_material_2_name_id_form = params["sub_material_2_name_id_form"]
-        @sub_material_3_name_id_form = params["sub_material_3_name_id_form"]
-        @sub_material_4_name_id_form = params["sub_material_4_name_id_form"]
+    @result_no_form = params["result_no_form"]
+    @generate_no_form = params["generate_no_form"]
+    @last_result_no_form = params["last_result_no_form"]
+    @last_generate_no_form = params["last_generate_no_form"]
+    @party_no_form = params["party_no_form"]
+    @e_no_form = params["e_no_form"]
+    @sub_no_form = params["sub_no_form"]
+    @result_i_no_form = params["result_i_no_form"]
+    @source_i_no_form = params["source_i_no_form"]
+    @main_material_i_no_form = params["main_material_i_no_form"]
+    @sub_material_1_i_no_form = params["sub_material_1_i_no_form"]
+    @sub_material_2_i_no_form = params["sub_material_2_i_no_form"]
+    @sub_material_3_i_no_form = params["sub_material_3_i_no_form"]
+    @sub_material_4_i_no_form = params["sub_material_4_i_no_form"]
+    @main_material_name_id_form = params["main_material_name_id_form"]
+    @sub_material_1_name_id_form = params["sub_material_1_name_id_form"]
+    @sub_material_2_name_id_form = params["sub_material_2_name_id_form"]
+    @sub_material_3_name_id_form = params["sub_material_3_name_id_form"]
+    @sub_material_4_name_id_form = params["sub_material_4_name_id_form"]
         
     show_sub_hash =  {"show_main"=> @show_main,"show_sub" => @show_sub}
     sub_no_set(params, show_sub_hash)
