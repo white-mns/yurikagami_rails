cdccbfbbb24307905efb90d35859bd4bfd4dbfb4
shiro <shiro.mns@gmail.com>
2018-04-26 18:12:51 +0900

    [追加]scaffoldで作成したファイルを調整。キャラクター情報、敵情報の関連付け

--- app/controllers/item_gets_controller.rb
+++ app/controllers/item_gets_controller.rb
@@ -5,8 +5,8 @@ class ItemGetsController < ApplicationController
   # GET /item_gets
   def index
     param_set
-    @count	= ItemGet.includes([:p_name]).search(params[:q]).result.count()
-    @search	= ItemGet.includes([:p_name]).page(params[:page]).search(params[:q])
+    @count	= ItemGet.includes([:p_name, :enemy_name]).search(params[:q]).result.count()
+    @search	= ItemGet.includes([:p_name, :enemy_name]).page(params[:page]).search(params[:q])
     @search.sorts = 'id asc' if @search.sorts.empty?
     @item_gets	= @search.result.per(50)
   end
@@ -17,29 +17,40 @@ class ItemGetsController < ApplicationController
     params[:q]  = params[:q] ? params[:q] : {}
     
     reference_text_assign(params, "p_name_name", "p_name_form")
-        reference_number_assign(params, "result_no", "result_no_form")
-        reference_number_assign(params, "generate_no", "generate_no_form")
-        reference_number_assign(params, "party_no", "party_no_form")
-        reference_number_assign(params, "e_no", "e_no_form")
-        reference_number_assign(params, "sub_no", "sub_no_form")
-        reference_number_assign(params, "enemy", "enemy_form")
-        reference_number_assign(params, "item", "item_form")
-        reference_number_assign(params, "is_pk", "is_pk_form")
+    reference_number_assign(params, "result_no", "result_no_form")
+    reference_number_assign(params, "generate_no", "generate_no_form")
+    reference_number_assign(params, "party_no", "party_no_form")
+    reference_number_assign(params, "e_no", "e_no_form")
+    reference_number_assign(params, "sub_no", "sub_no_form")
+    reference_text_assign(params, "enemy_name_name", "enemy_form")
+    reference_text_assign(params, "item", "item_form")
+    reference_number_assign(params, "is_pk", "is_pk_form")
         
     @p_name_form = params["p_name_form"]
-        @result_no_form = params["result_no_form"]
-        @generate_no_form = params["generate_no_form"]
-        @party_no_form = params["party_no_form"]
-        @e_no_form = params["e_no_form"]
-        @sub_no_form = params["sub_no_form"]
-        @enemy_form = params["enemy_form"]
-        @item_form = params["item_form"]
-        @is_pk_form = params["is_pk_form"]
+    @result_no_form = params["result_no_form"]
+    @generate_no_form = params["generate_no_form"]
+    @party_no_form = params["party_no_form"]
+    @e_no_form = params["e_no_form"]
+    @sub_no_form = params["sub_no_form"]
+    @enemy_form = params["enemy_form"]
+    @item_form = params["item_form"]
+    @is_pk_form = params["is_pk_form"]
         
     show_sub_hash =  {"show_main"=> @show_main,"show_sub" => @show_sub}
     sub_no_set(params, show_sub_hash)
     @show_main = show_sub_hash["show_main"]
     @show_sub = show_sub_hash["show_sub"]
+    
+    params[:q]["is_pk_eq_any"] = []
+    if params["is_pk"] == "on" then params[:q]["is_pk_eq_any"].push(1) end
+    if params["no_pk"] == "on" then params[:q]["is_pk_eq_any"].push(0) end
+    @is_pk = params["is_pk"]
+    @no_pk  = params["no_pk"]
+    if params[:q]["is_pk_eq_any"].size == 0 then 
+        @is_pk = "on"
+        @no_pk  = "on"
+    end
+
   end
   # GET /item_gets/1
   #def show
--- app/models/item_get.rb
+++ app/models/item_get.rb
@@ -1,2 +1,4 @@
 class ItemGet < ApplicationRecord
+	belongs_to :p_name,	    :foreign_key => [:e_no, :result_no, :sub_no, :generate_no], :primary_key => [:e_no, :result_no, :sub_no, :generate_no], :class_name => 'Name'
+	belongs_to :enemy_name, :foreign_key => :enemy,    :primary_key => :enemy_id, :class_name => 'EnemyName'
 end
