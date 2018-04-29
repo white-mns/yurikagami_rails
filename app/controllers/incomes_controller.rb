c4286917c3377b750410d36e9088ddf7ce6e95d3
shiro <shiro.mns@gmail.com>
2018-04-29 14:34:12 +0900

    [追加]scaffoldで作成したファイルを調整。キャラクター情報の関連付け

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -17,31 +17,46 @@ class IncomesController < ApplicationController
     params[:q]  = params[:q] ? params[:q] : {}
     
     reference_text_assign(params, "p_name_name", "p_name_form")
-        reference_number_assign(params, "result_no", "result_no_form")
-        reference_number_assign(params, "generate_no", "generate_no_form")
-        reference_number_assign(params, "party_no", "party_no_form")
-        reference_number_assign(params, "e_no", "e_no_form")
-        reference_number_assign(params, "sub_no", "sub_no_form")
-        reference_number_assign(params, "money", "money_form")
-        reference_number_assign(params, "sundries", "sundries_form")
-        reference_number_assign(params, "exp", "exp_form")
-        reference_number_assign(params, "is_pk", "is_pk_form")
+    reference_number_assign(params, "result_no", "result_no_form")
+    reference_number_assign(params, "generate_no", "generate_no_form")
+    reference_number_assign(params, "party_no", "party_no_form")
+    reference_number_assign(params, "e_no", "e_no_form")
+    reference_number_assign(params, "sub_no", "sub_no_form")
+    reference_number_assign(params, "money", "money_form")
+    reference_number_assign(params, "sundries", "sundries_form")
+    reference_number_assign(params, "exp", "exp_form")
+    reference_number_assign(params, "is_pk", "is_pk_form")
         
     @p_name_form = params["p_name_form"]
-        @result_no_form = params["result_no_form"]
-        @generate_no_form = params["generate_no_form"]
-        @party_no_form = params["party_no_form"]
-        @e_no_form = params["e_no_form"]
-        @sub_no_form = params["sub_no_form"]
-        @money_form = params["money_form"]
-        @sundries_form = params["sundries_form"]
-        @exp_form = params["exp_form"]
-        @is_pk_form = params["is_pk_form"]
+    @result_no_form = params["result_no_form"]
+    @generate_no_form = params["generate_no_form"]
+    @party_no_form = params["party_no_form"]
+    @e_no_form = params["e_no_form"]
+    @sub_no_form = params["sub_no_form"]
+    @money_form = params["money_form"]
+    @sundries_form = params["sundries_form"]
+    @exp_form = params["exp_form"]
+    @is_pk_form = params["is_pk_form"]
         
     show_sub_hash =  {"show_main"=> @show_main,"show_sub" => @show_sub}
     sub_no_set(params, show_sub_hash)
     @show_main = show_sub_hash["show_main"]
     @show_sub = show_sub_hash["show_sub"]
+    
+    params[:q]["is_pk_eq_any"] = []
+    if params["is_enemy"] == "on" then params[:q]["is_pk_eq_any"].push(0) end
+    if params["is_practice"] == "on" then params[:q]["is_pk_eq_any"].push(1) end
+    if params["is_pk"] == "on" then params[:q]["is_pk_eq_any"].push(2) end
+    
+    @is_pk = params["is_pk"]
+    @is_enemy  = params["is_enemy"]
+    @is_practice  = params["is_practice"]
+
+    if params[:q]["is_pk_eq_any"].size == 0 then 
+        @is_pk        = "on"
+        @is_enemy     = "on"
+        @is_practice  = "on"
+    end
   end
   # GET /incomes/1
   #def show
