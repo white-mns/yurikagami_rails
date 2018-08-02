4c9f16cfb085b1a5efc80e4badf6550f09ee667a
shiro <shiro.mns@gmail.com>
2018-08-03 03:49:09 +0900

    [変更]グラフの級間隔を最大値に応じて自動で判定するように

--- app/models/status.rb
+++ app/models/status.rb
@@ -4,7 +4,16 @@ class Status < ApplicationRecord
 	belongs_to :job1_name, :foreign_key => :job1, :primary_key => :job_id, :class_name => 'JobName'
 	belongs_to :job2_name, :foreign_key => :job2, :primary_key => :job_id, :class_name => 'JobName'
 
-    def self.to_range_graph(column, range=50, figure_length=5)
+    def self.to_range_graph(column)
+        max = self.pluck(column).max
+        figure_length = max.to_s.length
+
+        range = (max / 20).to_i
+        floor_num = (10 ** (range.to_s.length - 1))
+        range = (range / floor_num).to_i # 範囲の最上位桁の値を1,2,5のいずれかにする
+        range = range <= 2 ? range : 5
+        range = range * floor_num
+
         self.pluck(column).inject(Hash.new(0)){|hash, a| floor= (a/range).to_i()*range; hash[floor.to_s.rjust(figure_length) + "～" + (floor+range).to_s.rjust(figure_length)] += 1 ; hash}.sort
     end
     
