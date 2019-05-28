3e3ce38ed31c6c6f8a9b416e8214715f05f114c5
shiro <shiro.mns@gmail.com>
2019-05-28 22:50:09 +0900

    [追加]固有名詞一覧

---
+++ app/controllers/proper_names_controller.rb
@@ -0,0 +1,78 @@
+class ProperNamesController < ApplicationController
+  include MyUtility
+  before_action :set_proper_name, only: [:show, :edit, :update, :destroy]
+
+  # GET /proper_names
+  def index
+    placeholder_set
+    param_set
+    @count	= ProperName.notnil().includes(:pc_name).search(params[:q]).result.hit_count()
+    @search	= ProperName.notnil().includes(:pc_name).page(params[:page]).search(params[:q])
+    @search.sorts = "id asc" if @search.sorts.empty?
+    @proper_names	= @search.result.per(50)
+  end
+
+  def param_set
+    @form_params = {}
+
+    @latest_result = Name.maximum("result_no")
+
+    params_clean(params)
+    if !params["is_form"] then
+        params["result_no_form"] ||= sprintf("%d",@latest_result)
+    end
+
+    params_to_form(params, @form_params, column_name: "pc_name_name", params_name: "pc_name_form", type: "text")
+    params_to_form(params, @form_params, column_name: "proper_id", params_name: "proper_id_form", type: "number")
+    params_to_form(params, @form_params, column_name: "name", params_name: "name_form", type: "number")
+  end
+  # GET /proper_names/1
+  #def show
+  #end
+
+  # GET /proper_names/new
+  #def new
+  #  @proper_name = ProperName.new
+  #end
+
+  # GET /proper_names/1/edit
+  #def edit
+  #end
+
+  # POST /proper_names
+  #def create
+  #  @proper_name = ProperName.new(proper_name_params)
+
+  #  if @proper_name.save
+  #    redirect_to @proper_name, notice: "Proper name was successfully created."
+  #  else
+  #    render action: "new"
+  #  end
+  #end
+
+  # PATCH/PUT /proper_names/1
+  #def update
+  #  if @proper_name.update(proper_name_params)
+  #    redirect_to @proper_name, notice: "Proper name was successfully updated."
+  #  else
+  #    render action: "edit"
+  #  end
+  #end
+
+  # DELETE /proper_names/1
+  #def destroy
+  #  @proper_name.destroy
+  #  redirect_to proper_names_url, notice: "Proper name was successfully destroyed."
+  #end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_proper_name
+      @proper_name = ProperName.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def proper_name_params
+      params.require(:proper_name).permit(:proper_id, :name)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_133343) do
+ActiveRecord::Schema.define(version: 2019_05_28_135210) do
+
+  create_table "proper_names", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.integer "proper_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_proper_names_on_name"
+    t.index ["proper_id"], name: "index_proper_names_on_proper_id"
+  end
 
   create_table "uploaded_checks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.integer "result_no"
