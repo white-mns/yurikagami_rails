a41118fa1e4a0cb3164328ab96b810f252b0bc4c
shiro <shiro.mns@gmail.com>
2019-06-01 16:57:59 +0900

    [追加]トップページに表示する更新情報

--- app/controllers/top_page_controller.rb
+++ app/controllers/top_page_controller.rb
@@ -1,4 +1,7 @@
 class TopPageController < ApplicationController
   def index
+    @latest_result = Name.maximum("result_no")
+    @latest_generate = Name.where(result_no: @latest_result).maximum("generate_no")
+    @uploaded_result = UploadedCheck.maximum("result_no")
   end
 end
